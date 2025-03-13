b12394341146cb7526b0f17be42f567d3a82548b
danielgray791 <danielgray791@outlook.com>
2025-03-13 17:44:38 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,16 @@
-import { readFileSync } from 'fs';
-import { Page, chromium, Frame, Cookie, Browser, CDPSession, Route, BrowserContext } from 'patchright';
+import express, { Express, Request, Response } from "express";
+const app: Express  = express();
+import patchright, { Browser, Page, Frame, CDPSession, Route } from "patchright";
+import chromium from "@sparticuz/chromium-min";
 
-const OBFS = readFileSync('C:/xampp/htdocs/garutcapil/hcaptcha/turnstilebypasser/obfs.txt', 'utf-8');
-const SUNO_HTML = readFileSync('C:/xampp/htdocs/garutcapil/hcaptcha/turnstilebypasser/src/assets/suno.html', 'utf-8');
-const USER_AGENT_SELECTED = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36`;
-const CHROMIUM_EXECUTABLE_PATH = `C:/Users/Muhammad Raivaldy/AppData/Local/ms-playwright/chromium-1155/chrome-win/chrome.exe`;
+const userAgent = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36`;
+const obfsScriptUrl = `https://pastebin.com/raw/VVdu3Equ`;
+const sunoHtmlUrl = `https://pastebin.com/raw/ym4HxgZE`;
+const remoteExecutablePath =
+ `https://github.com/Sparticuz/chromium/releases/download/v133.0.0/chromium-v133.0.0-pack.tar`;
+let browser: patchright.Browser;
+let obfsScriptText: string;
+let sunoHtmlText: string;
 
 function getFrame(url: string | RegExp, page: Page): Frame | null {
     const frames = page.frames();
@@ -22,20 +28,9 @@ function getFrame(url: string | RegExp, page: Page): Frame | null {
     return null;
 }
 
-async function initBrowser(headless: boolean = true): Promise<[Browser, Page, CDPSession]> {
-    const browser = await chromium.launch({
-        args: [
-            `--user-agent=${USER_AGENT_SELECTED}`
-        ],
-        headless: headless,
-        executablePath: CHROMIUM_EXECUTABLE_PATH
-    });
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    const cdpSession = await page.context().newCDPSession(page);
-    await page.addInitScript(OBFS);
-
-    return [browser, page, cdpSession];
+async function getPastebinContent(url: string): Promise<string> {
+    const res = await fetch(url);
+    return await res.text();
 }
 
 // Fungsi sleep yang menerima waktu dalam milidetik
@@ -43,18 +38,39 @@ function sleep(ms: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-export async function getTurnstileToken(headless: boolean = false) {
-    return new Promise<string | undefined> (async function (resolve, reject) { 
-        const url = "https://suno.com/"; 
-        const [browser, page, cdpSession] = await initBrowser(headless);
+async function initBrowser(): Promise<[Browser, Page, CDPSession]> {
+    if (browser === undefined) {
+        browser = await patchright.chromium.launch({
+            args: [...chromium.args, `--user-agent=${userAgent}`],
+            executablePath: await chromium.executablePath(remoteExecutablePath),
+            headless: true,
+        });
+    }
+           
+    const page = await browser.newPage();
+    const cdpSession = await page.context().newCDPSession(page);
+    if (!obfsScriptText && !sunoHtmlText) {
+        obfsScriptText = await getPastebinContent(obfsScriptUrl);
+        sunoHtmlText = await getPastebinContent(sunoHtmlUrl);
+    }
+    await page.addInitScript(obfsScriptText);
+
+    return [browser, page, cdpSession];
+}
+
+app.get("/", async function (req, res) {
+    try {
+        const url = "https://suno.com";
+        const [browser, page, cdpSession] = await initBrowser();
 
         await page.route(url, async function (route: Route) {
-            // console.log(route.request().method(), route.request().url());
+            console.log(route.request().method(), route.request().url());
             if (route.request().method() === "GET") {
-                route.fulfill({body: SUNO_HTML, status: 200})
+                route.fulfill({body: sunoHtmlText, status: 200})
             }
         })
         await page.goto(url);
+
         var start = Date.now();
 
         while(true) {
@@ -71,33 +87,40 @@ export async function getTurnstileToken(headless: boolean = false) {
                 const token = await tokenElement.inputValue();
                 
                 if (token) {
-                    await browser.close();
-                    // console.log(token);
-                    resolve(token);
+                    await page.close();
+                    console.log("Token terlihat");
+                    console.log(token);
+                    res.status(200).send({token: token})
                     break;
                 }
     
                 const turnstileFrame = getFrame(/https:\/\/challenges.*auto\//, page);
                 if (turnstileFrame) {
-                    // console.log("Turnstile frame terdeteksi");
+                    console.log("Turnstile frame terdeteksi");
                     await turnstileFrame.click("body", {delay: 1500});   
-                    // console.log("Berhasil mengklik!");  
+                    console.log("Berhasil mengklik!");  
                 }
             } catch (err) {
-                // console.log(`Exception: ${err}`);
+                console.log(`Exception: ${err}`);
             }
 
             await sleep(1000);
         }
-    });
-}
 
-async function main() {
-    const token = await getTurnstileToken(false);
-    // console.log(token)
-}
 
-(async () => {
-    await main();
-})();
-// main()
+        // const pageContent = await page.content();
+        // await browser.close();
+        // res.send(await page.evaluate(function() { return navigator.userAgent }));
+    } catch (err) {
+        res.status(500).send("Error" + err);
+    }
+
+});
+
+app.listen(3000, function() { 
+    console.log("Server ready on port 3000.")
+});
+
+module.exports = app;
+
+// Jqawz9m8hpFHllaKqYmlFzmD
